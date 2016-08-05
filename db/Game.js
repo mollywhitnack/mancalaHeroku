f110182e58a04359f9adfc4db7f7de7555e95848
mollywhitnack <mollywhitnack@gmail.com>
2016-08-05 13:23:16 -0700

    mongoosE

--- db/Game.js
+++ db/Game.js
@@ -1,15 +1,13 @@
-import mongoose, { Schema } from 'mongoose'
+const mongoose = require('mongoose')
+let Schema = mongoose.Schema
 
-let Game;
 
-let gameSchema = Schema({
+const gameSchema = Schema({
   currPlayer: {type: Boolean, default: false},
   gameboard : {type: [], default: [0,4,4,4,4,4,4,0,4,4,4,4,4,4] }, //[new Array(14).fill(0)] }
   player0: {type: String, default: 'player 1'},
   player1: {type: String, defualt: 'player 2'},
 });
 
-
-Game = mongoose.model('Game', gameSchema);
-
-export default Game;
+let Game  = mongoose.model('Game', gameSchema);
+module.exports = Game;
